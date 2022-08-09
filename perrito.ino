4d52de6ee3e6a161bba66200c91b18acaa69c9e1
EliasMichael2 <102168318+EliasMichael2@users.noreply.github.com>
2022-08-09 14:02:54 -0300

    es el codigo de los servos

---
+++ perrito.ino
@@ -0,0 +1,31 @@
+
+#include <Servo.h>
+ 
+
+Servo servoMotor;
+ 
+void setup() {
+ 
+  Serial.begin(9600);
+ 
+
+  servoMotor.attach(9);
+}
+ 
+void loop() {
+  
+
+  servoMotor.write(0);
+
+  delay(1000);
+  
+
+  servoMotor.write(30);
+ 
+  delay(1000);
+  
+ 
+  servoMotor.write(0);
+ 
+  delay(1000);
+}
